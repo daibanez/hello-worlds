1fd9709b7deaf47e1d1148de90cca155a60700c8
z594 <z594.email@gmail.com>
2017-08-16 20:09:13 -0500

    Flip flop on c# impl

--- c-sharp/src/HelloWorld/HelloWorld.cs
+++ c-sharp/src/HelloWorld/HelloWorld.cs
@@ -4,20 +4,38 @@ namespace HelloWorld
 {
 	class HelloWorld
 	{
+
+		public static readonly string helloTemplate;
+		public static readonly string defaultUser;
+
+		static UserMessage()
+		{
+			helloTemplate = "Hello {0}!";
+			defaultUser = "World";
+		}
+
+		public static string SayHello()
+		{
+			return SayHello(defaultUser);
+		}
+
+		public static string SayHello(string name)
+		{
+			return String.Format(helloTemplate,name);
+		}
+
 		static void Main(string[] args)
 		{
-			if (args.Length > 0)
+			if (args.length > 0)
 			{
-				for(int i = 0; i < args.Length; i++)
+				for (int i = 0; i < args.Length; i++)
 				{
-					UserMessage msg = new UserMessage(args[i]);
-					Console.WriteLine(msg);
+					Console.WriteLine(SayHello(s));
 				}
 			}
 			else
 			{
-				UserMessage msg = new UserMessage();
-				Console.WriteLine(msg);
+				Console.WriteLine(SayHello());
 			}
 
 		}
