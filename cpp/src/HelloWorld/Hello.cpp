464b23ff7809216f11a00f74f5278c11c58c18f2
z594 <z594.email@gmail.com>
2017-08-17 21:03:05 -0500

    Add c++ hello class

---
+++ cpp/src/HelloWorld/Hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+namespace HelloWorld {
+
+	string Hello::helloTemplate = "Hello %s!";
+	string Hello::defaultUser = "World";
+
+	string Hello:sayHello(string name = defaultUser) {
+		return 
+	}
+
+}
---
+++ cpp/src/HelloWorld/Hello.hpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+namespace HelloWorld {
+
+	class Hello {
+
+	public:
+		static string helloTemplate;
+		static string defaultUser;
+
+		string sayHello(string name = defaultUser)
+
+	}
+
+}
